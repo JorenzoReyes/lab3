ce29c124a89ae93643f85a7b0d3cca8441dbfa8f
Jorenzo Martin Reyes <jjreyes@student.apc.edu.ph>
2024-03-07 15:07:12 +0800

    added news

--- ci4/app/Config/Routes.php
+++ ci4/app/Config/Routes.php
@@ -1,14 +1,19 @@
 <?php
 
-use CodeIgniter\Router\RouteCollection;
 
+use App\Controllers\News; // Add this line
 use App\Controllers\Pages;
+use App\Controllers\Guests;
 
-/**
- * @var RouteCollection $routes
- */
+$routes->get('news', [News::class, 'index']);           // Add this line
+$routes->get('news/new', [News::class, 'new']); // Add this line
+$routes->post('news', [News::class, 'create']); // Add this line
+$routes->get('news/(:segment)', [News::class, 'show']); // Add this line
 
-$routes->get('/', 'Home::index');
+$routes->get('guests', [Guests::class, 'index']);           // Add this line
+$routes->get('guests/new', [Guests::class, 'new']); // Add this line
+$routes->post('guests', [Guests::class, 'create']); // Add this line
+$routes->get('guests/(:segment)', [Guests::class, 'show']); // Add this line
 
 
 $routes->get('pages', [Pages::class, 'index']);
